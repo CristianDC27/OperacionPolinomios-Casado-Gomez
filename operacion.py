6bbe86db741f000af535d06b3a6dc33f9b54bec2
Cristian David Casado <cristiancasado27@gmail.com>
2022-04-13 17:24:44 +0200

    Class extracted

---
+++ operacion.py
@@ -0,0 +1,15 @@
+class Operacion:
+    def __init__(self, polinomio1, polinomio2):
+        self.polinomio1 = polinomio1
+        self.polinomio2 = polinomio2
+        return
+    def operar(self, operacion):
+        if operacion == "+":
+            return self.suma()
+        if operacion == "-":
+            return self.res()
+    def res(self):
+        return self.polinomio1 - self.polinomio2
+    def suma(self):
+	    return self.polinomio1 + self.polinomio2
+
--- operacionesPolinomios.py
+++ operacionesPolinomios.py
@@ -1,46 +1,42 @@
-"""
-Algortimo para la resolucion de operaciones entre 2 polinomios
+"""Algortimo para la resolucion de operaciones entre 2 polinomios
+
 Ejercicio: Resolver la siguiente multiplicación de polinomios
 P(x) = 7*x**3 + 2*x**2 + x - 7
 Q(x) = x**2 + 3
+
 """
 
 #Importamos la libreria SympY para usar variables simbolicas (x, y)
 import sympy
 
-#Obtenes los dos polinomios introducidos por el usuario
-P1 = input("Primer Polinomio: ")
-P2 = input("Segundo Polinomio: ")
+from operacion import Operacion
 
-#Definimos los simbolos
-sympy.init_printing()
-x,y = sympy.symbols('x,y')
+while True:
+   try:
+      print("Ejemplo de formato: 7*x**3 + 2*x**2 + x - 7\n")
+      #Obtenes los dos polinomios introducidos por el usuario
+      P1 = input("Primer Polinomio: ")
+      P2 = input("Segundo Polinomio: ")
 
-#Luego almacenamos en varibles los dos polinomios procesados por la funcion Poly de sympy
-Poly1 = sympy.Poly(P1)
-Poly2 = sympy.Poly(P2)
+      #Definimos los simbolos
+      sympy.init_printing()
+      x,y = sympy.symbols('x,y')
+
+      #Luego almacenamos en varibles los dos polinomios procesados por la funcion Poly de sympy
+      Poly1 = sympy.Poly(P1)
+      Poly2 = sympy.Poly(P2)
+
+      break
+
+   except:
+      print("Formato erróneo\n")
+
+operacion = Operacion(Poly1, Poly2)
 
 # Dar opcion al usuario a sumar o restar
 while True:
     option = input("Introduzca la operación a realizar '+' o '-': ")
     if option == "+" or option =="-":
         break
-#Declaramos una funcion para cada operacion que querramos utilizar
 
-def suma(p1, p2):
-	return p1 + p2
-
-def res(p1, p2):
-	return p1 - p2
-
-#Guardamos los valores retornados por las funciones y les pasamos los 2 polinomios como parametros,  Poly1 y Poly2
-if option == "+":
-   resultSuma = suma(Poly1, Poly2)
-if option == "-":
-   resultRes = res(Poly1, Poly2)
-
-#Mostramos el valor que deseemos
-if option == "+":
-   print("Resultado suma: ", resultSuma)
-if option == "-":
-   print("Resultado resta: ", resultRes)
+print("Resultado: ", operacion.operar(option))
